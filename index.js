309b23a7eaffbbba680d39e162a7533216d7c9a5
Leozin2705 <127213255+Leozin2705@users.noreply.github.com>
2024-09-05 01:19:48 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let anoAtual = 2024;
+const nomeCompleto = "Leonardo lopes ";
+let idade = 17;
+const cursoFaculdade = "Engenharia ";
+let cidadeAtual = "SJP";
+
+console.log(`Olá, eu sou a ${nomeCompleto} eu tenho $`);
+
+anoAtual = 2025;
+
+console.log(anoAtual);
